18daaa14dc50daa448d23e1d84c1c00991821cd1
John Ryan <john.p.ryan4@gmail.com>
2015-05-10 12:31:28 -0500

    run dartfmt

--- bin/detector.dart
+++ bin/detector.dart
@@ -11,14 +11,22 @@ class Configuration {
   String repoName;
 }
 
+RepositorySlug makeRepoSlug(Configuration config) =>
+    new RepositorySlug(config.accountName, config.repoName);
+
 /// Given a github repository, finds all open PRs, and attempts to merge
 /// each PR.  If there are any merge conflicts, returns the previous
 /// successful branch that was merged, and the branch that caused the
 /// merge conflict.
 main(List<String> args) async {
   var config = await getConfiguration(args);
-  var githubClient = createGitHubClient(auth: new Authentication.withToken(config.authToken));
-  githubClient.pullRequests.list(new RepositorySlug(config.accountName, config.repoName)).listen((pr) {
+  var auth = new Authentication.withToken(config.authToken);
+  var githubClient = createGitHubClient(auth: auth);
+
+  // make a list of pull requests
+  var repoStream = githubClient.pullRequests.list(makeRepoSlug(config));
+  var repoList = await repoStream.toList();
+  repoList.forEach((PullRequest pr) {
     print("""
       PR title: ${pr.title}
       Github Repo User: ${pr.base.user.login}
@@ -26,6 +34,7 @@ main(List<String> args) async {
       branch name: ${pr.base.ref}
     """);
   });
+  exit(0);
 }
 
 /// exits if incorrect arguments were given
@@ -48,8 +57,7 @@ Future<Configuration> getConfiguration(List<String> args) async {
 Future<String> getAuthToken() async {
   var homeDir = Platform.environment['HOME'];
   var configFile = new File('$homeDir/.merge_conflict_detector_config');
-  if (await configFile.exists())
-    return configFile.readAsString();
+  if (await configFile.exists()) return configFile.readAsString();
   print('no config file found.  Enter your github auth token:');
   var authToken = stdin.readLineSync();
   configFile.writeAsString(authToken);
